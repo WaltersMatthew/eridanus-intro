2b886d1c73799f6fc3c720234a2b2b742f3dbaa8
waltersmatthew <waltersmatthew88@gmail.com>
2023-03-09 15:57:23 -0800

    add stretch goals

--- js/index.js
+++ js/index.js
@@ -30,15 +30,15 @@ messageForm.addEventListener("submit", (event) => {
 
     const usersName = event.target.usersName.value;
     const usersEmail = event.target.usersEmail.value;
-    const usersMessage = event.target.usersMessage.value;
+    let usersMessage = event.target.usersMessage.value;
 
     const messageSection = document.getElementById("messages");
     const messageList = messageSection.querySelector("ul");
-    const newMessage = document.createElement("li");
+    let newMessage = document.createElement("li");
 
     newMessage.innerHTML = `
-        <a href = "mailto:${usersEmail}">${usersName}</a>
-        <span>says: ${usersMessage}</span>
+    <a href = "mailto:${usersEmail}">${usersName}</a>
+    <span>says: ${usersMessage}</span>
     `;
 
     const removeButton = document.createElement("button");
@@ -48,10 +48,46 @@ messageForm.addEventListener("submit", (event) => {
     removeButton.addEventListener("click", (event) => {
         const entry = event.target.parentNode;
         entry.remove();
+        if (messageList.childElementCount === 0) {
+            messageSection.style.display = "none";
+        }
     });
 
+    messageSection.style.display = "block";
     newMessage.appendChild(removeButton);
     messageList.appendChild(newMessage);
+
+    const editButton = document.createElement("button");
+    editButton.textContent = "edit";
+    editButton.type = "button";
+
+    const editForm = document.createElement("form");
+    const editInput = document.createElement("input");
+    editInput.type = "text";
+    editInput.value = usersMessage;
+    const saveButton = document.createElement("button");
+    saveButton.textContent = "Save";
+    saveButton.type = "submit";
+    editForm.append(editInput, saveButton);
+
+    editButton.addEventListener("click", () => {
+        newMessage.replaceWith(editForm);
+    });
+
+    editForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        usersMessage = editInput.value;
+        newMessage.innerHTML = `
+        <a href = "mailto:${usersEmail}">${usersName}</a>
+        <span>says: ${usersMessage}</span>
+        `;
+        newMessage.appendChild(removeButton);
+        newMessage.appendChild(editButton);
+        messageList.appendChild(newMessage);
+        editForm.remove();
+    });
+
+    newMessage.appendChild(editButton);
 
     messageForm.reset();
 });
